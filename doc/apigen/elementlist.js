da00fa5ef0dc88fc387369ab402875f68d252217
Hajime MATSUMOTO <mail@hazime.org>
2013-02-10 19:06:34 +0900

    Document Updates

--- doc/apigen/elementlist.js
+++ doc/apigen/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Nora\\Bootstrap\\Bootstrapper"],["c","Nora\\Bootstrap\\ResourceIF"],["c","Nora\\Container\\Container"],["c","Nora\\Container\\WithContainer"],["c","Nora\\Core\\LibraryLoader"],["c","Nora\\Core\\Nora"],["c","Nora\\Core\\Singleton"],["c","Nora\\DI\\DI"],["c","Nora\\DI\\ServiceIF"],["c","NoraResource\\Request"]];
+ApiGen.elements = [["c","Nora\\Bootstrap\\Bootstrapper"],["c","Nora\\Bootstrap\\ResourceIF"],["c","Nora\\Container\\Container"],["c","Nora\\Container\\WithContainer"],["c","Nora\\Core\\AutoConfig"],["c","Nora\\Core\\LibraryLoader"],["c","Nora\\Core\\Nora"],["c","Nora\\Core\\Singleton"],["c","Nora\\DI\\ConfigureIF"],["c","Nora\\DI\\Container"],["c","Nora\\DI\\FactoryIF"],["c","NoraResource\\Request"]];
